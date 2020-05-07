9a919961104eae493e92fd7f2d000c0b6cb2236e
Nemolite <g25092011@mail.ru>
2020-05-07 21:37:03 +0300

    inp

--- js/recoun.js
+++ js/recoun.js
@@ -1,5 +1,4 @@
 
-console.log(111)
 var app = new Vue({
         el: '.recount-content',
         data: {
@@ -11,3 +10,28 @@ var app = new Vue({
             }
         }
     })
+
+
+
+$(function() {
+ 
+  (function quantityProducts() {
+    var $quantityArrowMinus = $(".quantity-arrow-minus");
+    var $quantityArrowPlus = $(".quantity-arrow-plus");
+    var $quantityNum = $(".quantity-num");
+ 
+    $quantityArrowMinus.click(quantityMinus);
+    $quantityArrowPlus.click(quantityPlus);
+ 
+    function quantityMinus() {
+      if ($quantityNum.val() > 1) {
+        $quantityNum.val(+$quantityNum.val() - 1);
+      }
+    }
+ 
+    function quantityPlus() {
+      $quantityNum.val(+$quantityNum.val() + 1);
+    }
+  })();
+ 
+});
